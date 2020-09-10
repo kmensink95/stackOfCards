3fab309cf85ff3628d616ae5a8391c1f64725eb9
kmensink95 <kmensink95@gmail.com>
2020-09-10 10:19:11 +0200

    create prototype to display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { pullStack, pushStack } from './utils/slider';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'stackOfCards';
+export class AppComponent implements OnInit {
+  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
 }
