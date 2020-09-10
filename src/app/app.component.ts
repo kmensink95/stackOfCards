84dbbc59c956943107dcc52b712588a4a860b3f5
kmensink95 <kmensink95@gmail.com>
2020-09-10 10:34:58 +0200

    reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,8 @@ import { pullStack, pushStack } from './utils/slider';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
   constructor(){}
 
   ngOnInit(): void {
-    
   }
 }
--- src/app/utils/slider.ts
+++ src/app/utils/slider.ts
@@ -1,40 +1,36 @@
 export function pullStack(items: any[], lockMap: number[]): any {
-    const [first, ...rest] = items;
-    const newStack = [first, ...rest, items[findFirstUnlocked(items, lockMap)]];
-    return newStack.reduce((shifted, item, index) => {
-      if (lockMap.includes(index)) {
-        return [...shifted, item];
-      }
-      return [...shifted, newStack[findRightUnlocked(index, lockMap)]];
-    }, []).slice(0, -1);
-  }
-
-  //First next unlocked item
-  export function findRightUnlocked(index: number, lockMap: number[]): number {
-    let nextUnlockedIndex = ++index;
-    while (lockMap.includes(nextUnlockedIndex)) {
-      nextUnlockedIndex++;
+  const [first, ...rest] = items;
+  const newStack = [first, ...rest, items[findFirstUnlocked(items, lockMap)]];
+  return newStack.reduce((shifted, item, index) => {
+    if (lockMap.includes(index)) {
+      return [...shifted, item];
     }
-    return nextUnlockedIndex;
+    return [...shifted, newStack[findRightUnlocked(index, lockMap)]];
+  }, []).slice(0, -1);
+}
+//First next unlocked item
+export function findRightUnlocked(index: number, lockMap: number[]): number {
+  let nextUnlockedIndex = ++index;
+  while (lockMap.includes(nextUnlockedIndex)) {
+    nextUnlockedIndex++;
   }
-
-  export function findFirstUnlocked(items: number[], lockMap: number[]): number {
-    if (!lockMap.length) {
-      return 0;
-    }
-
-    for (let i = 0; i < items.length; i++) {
-      if (!lockMap.includes(i)) {
-        return i;
-      }
+  return nextUnlockedIndex;
+}
+export function findFirstUnlocked(items: number[], lockMap: number[]): number {
+  if (!lockMap.length) {
+    return 0;
+  }
+  for (let i = 0; i < items.length; i++) {
+    if (!lockMap.includes(i)) {
+      return i;
     }
   }
-
-  export function pushStack(items: any, lockMap: number[]): any {
-    const reverseArray = items.reverse();
-    let newLockMap = [];
-    lockMap.forEach(index => {
-      newLockMap = [...newLockMap, (items.length - 1) - index];
-    })
-    return pullStack(reverseArray, newLockMap).reverse();
-  }
+}
+export function pushStack(items: any, lockMap: number[]): any {
+  const reverseArray = items.reverse();
+  let newLockMap = [];
+  lockMap.forEach(index => {
+    newLockMap = [...newLockMap, (items.length - 1) - index];
+  })
+  return pullStack(reverseArray, newLockMap).reverse();
+}
