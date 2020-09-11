c6c5b23bb0709ae5a02d6bdd1676fdcc9fcce467
kmensink95 <kmensink95@gmail.com>
2020-09-11 13:58:38 +0200

    implement swiper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AmountPipe } from './pipes/amount.pipe';
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto'
+};
 
 @NgModule({
   declarations: [
@@ -10,9 +18,15 @@ import { AmountPipe } from './pipes/amount.pipe';
     AmountPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SwiperModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
